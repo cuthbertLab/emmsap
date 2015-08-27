fc3de1ae28634a7c53795889c14454c3fe16d5d4
Michael Scott Cuthbert <cuthbert@mit.edu>
2015-08-27 13:40:34 -0400

    models looking better

--- emmsapDJ/models.py
+++ emmsapDJ/models.py
@@ -12,37 +12,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Chords(models.Model):
-    pieceid = models.IntegerField(db_column='pieceId')  # Field name made lowercase.
-    chorddata = models.CharField(db_column='chordData', max_length=32765, blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'chords'
-
-
-class Composers(models.Model):
-    iscanonical = models.IntegerField(db_column='isCanonical', blank=True, null=True)  # Field name made lowercase.
-    canonicallink = models.IntegerField(db_column='canonicalLink', blank=True, null=True)  # Field name made lowercase.
-    name = models.CharField(max_length=64, blank=True, null=True)
-    sortyear = models.IntegerField(db_column='sortYear', blank=True, null=True)  # Field name made lowercase.
-    earliestyear = models.IntegerField(db_column='earliestYear', blank=True, null=True)  # Field name made lowercase.
-    latestyear = models.IntegerField(db_column='latestYear', blank=True, null=True)  # Field name made lowercase.
-    countryid = models.IntegerField(db_column='countryId', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'composers'
-
-
-class Countries(models.Model):
-    name = models.CharField(max_length=40, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'countries'
-
-
 class DjangoMigrations(models.Model):
     app = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
@@ -53,10 +22,52 @@ class DjangoMigrations(models.Model):
         db_table = 'django_migrations'
 
 
+
+class Chords(models.Model):
+    pieceid = models.IntegerField(db_column='pieceId')  # Field name made lowercase.
+    chorddata = models.CharField(db_column='chordData', max_length=32765, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'chords'
+
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=40, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'country'
+
+
+class Composer(models.Model):
+    iscanonical = models.BooleanField(db_column='isCanonical', blank=True, null=True)  # Field name made lowercase.
+    canonicallink = models.IntegerField(db_column='canonicalLink', blank=True, null=True)  # Field name made lowercase.
+    name = models.CharField(max_length=64, blank=True, null=True)
+    sortyear = models.IntegerField(db_column='sortYear', blank=True, null=True)  # Field name made lowercase.
+    earliestyear = models.IntegerField(db_column='earliestYear', blank=True, null=True)  # Field name made lowercase.
+    latestyear = models.IntegerField(db_column='latestYear', blank=True, null=True)  # Field name made lowercase.
+    country = models.ForeignKey(Country) 
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'composer'
+
+
+
 class Intervals(models.Model):
     fn = models.CharField(max_length=255, blank=True, null=True)
     partid = models.IntegerField(db_column='partId', blank=True, null=True)  # Field name made lowercase.
     intervals = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fn + ':' + str(self.partid)
+
 
     class Meta:
         managed = False
@@ -74,10 +85,50 @@ class Piecetwopart3Grammapping(models.Model):
         db_table = 'pieceTwoPart3GramMapping'
 
 
-class Pieces(models.Model):
+class Piece(models.Model):
     filename = models.CharField(unique=True, max_length=255, blank=True, null=True)
     piecename = models.CharField(max_length=255, blank=True, null=True)
     composer_id = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.filename
+
+    def stream(self):
+        '''
+        returns a music21 Score object from this piece
+        '''
+        if hasattr(self, '_stream') and self._stream is not None:
+            return self._stream
+        if self.filename is not None:
+            from music21 import converter
+            import os
+            from emmsap import files
+            try:
+                s = converter.parse(os.path.join(files.emmsapDir, self.filename))
+                self._stream = s
+                return s
+            except:
+                print("%s Failed in conversion" % self.filename)
+                return None
+        else:
+            return None
+    
+    def numberOfVoices(self):
+        '''
+        returns the number of voices in the piece
+        
+        >>> p = Piece(4)
+        >>> p.filename
+        u'Ascoli_Piceno_Mater_Digna_Dei_Lux.xml'
+        >>> p.numberOfVoices()
+        2
+        '''
+        s = self.stream()
+        if s is not None:
+            return len(s.parts)
+        else:
+            return 0
+
 
     class Meta:
         managed = False
--- emmsapDJ/settings.py
+++ emmsapDJ/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'emmsapDJ.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqll': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'kassia': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cuthbert',
         'USER': 'cuthbert',
