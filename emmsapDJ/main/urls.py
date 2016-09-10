827593bb317de2a9ba7de869f8cf07b687d31568
Michael Scott Cuthbert <cuthbert@mit.edu>
2016-09-10 14:53:36 -0400

    an app that does SOMETHING!

---
+++ emmsapDJ/main/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'emmsap'
+urlpatterns = [
+    # ex: /emmsap/
+    url(r'^$', views.index, name='index'),
+    # ex: /emmsap/composer/5/
+    url(r'^composer/(?P<composerId>[0-9]+)/', views.composer, name='composer'),
+    # ex: /emmsap/composer/list/
+    url(r'^composer/list/', views.listComposers, name='listComposers'),
+
+    # ex: /emmsap/piece/193/assignComposer/  
+    url(r'^piece/(?P<pieceId>[0-9]+)/assignComposer/$', views.assignComposer, name='assignComposer'),
+    url(r'^piece/(?P<pieceId>[0-9]+)/assignComposerFollowup/$', views.assignComposerFollowup, name='assignComposerFollowup'),
+]
--- emmsapDJ/main/views.py
+++ emmsapDJ/main/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+from . import models
 
 # Create your views here.
+
+def index(r):
+    return render(r, 'main/index.html', {
+       'allComposers': models.Composer.objects.order_by('name'),
+    })
+
+def how_to_do_index_the_long_way(request):
+    allComposers = models.Composer.objects.order_by('name')
+    template = loader.get_template('main/index.html')
+    context = {
+       'allComposers': allComposers,
+    }                  
+    return HttpResponse(template.render(context, request))
+
+
+def composer(request, composerId):
+    c = get_object_or_404(models.Composer, pk=composerId)    
+    allPieces = models.Piece.objects.filter(composer=c)
+    
+    return render(request, 'main/composerDetail.html', {'composer': c,
+                                                        'allPieces': allPieces,
+                                                        })
+
+def listComposers(request):
+    allComposers = models.Composer.objects.order_by('name')
+    out = '<br>\n'.join([c.name for c in allComposers])
+    return HttpResponse(out)
+
+def onePieceWithImage(r):
+    pass
+
+def assignComposer(r, pieceId):
+    p = get_object_or_404(models.Piece, pk=pieceId)
+    allComposers = models.Composer.objects.all()
+    return render(r, 'main/assignComposers.html', {'piece': p,
+                                                   'allComposers': allComposers,
+                                                   })
+
+def assignComposerFollowup(r, pieceId):
+    p = get_object_or_404(models.Piece, pk=pieceId)
+    try:
+        selectedComposer = models.Composer.objects.get(pk=r.POST['composerChoice'])
+    except (KeyError, models.Composer.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(r, 'main/assignComposers.html', {
+            'piece': p,
+            'error_message': "Your composer choice sucked!",
+        })
+    else:
+        p.composer = selectedComposer
+        p.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('emmsap:index'))    
+    
