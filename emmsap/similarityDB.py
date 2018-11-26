47815277fa36b7e093014a1a3038613ba62bef0e
Michael Scott Cuthbert <cuthbert@mit.edu>
2018-11-26 09:28:41 -0500

    add Janke SL OMR

--- emmsap/similarityDB.py
+++ emmsap/similarityDB.py
@@ -13,7 +13,7 @@ skipFileNames = [
                  ]
 
 class SimilaritySearcher(object): 
-    def __init__(self, startPiece=3453, endPiece=4000, minThreshold=6500, maxToShow=2):
+    def __init__(self, startPiece=3471, endPiece=4000, minThreshold=6500, maxToShow=2):
         self.dbObj = mysqlEM.EMMSAPMysql()
         self.startPiece = startPiece
         self.endPiece = endPiece
