6c28f9d4fe9e61fc67235bbb5172c4670e3199fd
Myke Cuthbert <cuthbert@gmail.com>
2021-05-07 17:16:05 -1000

    add dufay v6 3-9

---
+++ emmsap/meter_classification.py
@@ -0,0 +1,7 @@
+from files import FileIterator
+
+francesco_works_in_modus = [
+    1, 2, 3, 5, 6, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 20,
+    21,  # ?? not indicated but similar notation
+    22, 24, 25, 27, 28, 29, 34, 35, 36, 39,
+]
