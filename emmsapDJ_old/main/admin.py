2c06b2413991617bcb31f450251534327867bfbd
Michael Scott Asato Cuthbert <michael.asato.cuthbert@gmail.com>
2023-07-13 07:41:48 -1000

    remove emmsapDJ_old

--- emmsapDJ_old/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Piece, Composer, Country
-
-class CountryAdmin(admin.ModelAdmin):
-    model = Country
-
-admin.site.register(Piece)
-admin.site.register(Country, CountryAdmin)
-admin.site.register(Composer)
