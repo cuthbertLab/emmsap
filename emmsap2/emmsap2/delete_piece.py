635c80c18565bc9942599f5c93ec1d66b944d8e7
Michael Scott Asato Cuthbert <michael.asato.cuthbert@gmail.com>
2025-08-14 13:13:44 -1000

    add delete_missing_files util

--- emmsap2/emmsap2/delete_piece.py
+++ emmsap2/emmsap2/delete_piece.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from send2trash import send2trash
 
 from .models import Piece
+from .files import emmsapDir
 
 
 def run(piece_id_or_fn: int|str):
@@ -16,5 +17,24 @@ def run(piece_id_or_fn: int|str):
     if fn.exists():
         send2trash(fn)
     p.delete()
-    
+
+
+def delete_missing_files():
+    '''
+    In case a file has been renamed/removed on another copy of EMMSAP, you might
+    end up with phantom Piece objects with all their ratios etc.
+
+    This deletes all Piece objects and their cascading parts
+    if their file does not exist
+    '''
+    # little check.
+    if not emmsapDir.exists():
+        raise ValueError('Emmsap directory does not exist: would delete everything!')
+
+    tot_deleted = 0
+    for p in Piece.objects.all():
+        if not p.filepath.exists():
+            p.delete()
+            tot_deleted += 1
+    print(f'Deleted {tot_deleted} pieces.')
 
