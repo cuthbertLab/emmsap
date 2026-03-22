3650df8fdb824fcf69deef2216a3d1a214b21bca
Michael Scott Asato Cuthbert <michael.asato.cuthbert@gmail.com>
2026-03-21 15:48:32 -1000

    add command to searchDB

---
+++ emmsap2/emmsap2/management/commands/searchDB.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from ...models import Piece
+from ...similarity_ratio import SimilaritySearcher
+
+
+class Command(BaseCommand):
+    help = 'Search a range of pieces with SimilaritySearcher'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--start',
+            dest='start_piece',
+            type=int,
+            help='First piece ID to search',
+        )
+        parser.add_argument(
+            '--end',
+            dest='end_piece',
+            type=int,
+            help='Last piece ID to search (inclusive)',
+        )
+        parser.add_argument(
+            '--segment-type',
+            default='dia_rhy',
+            help='Segment encoding type to search',
+        )
+        parser.add_argument(
+            '--min-threshold',
+            type=int,
+            default=8000,
+            help='Minimum similarity threshold',
+        )
+
+    def handle(self, *args, **options):
+        start_piece = options['start_piece']
+        end_piece = options['end_piece']
+        segment_type = options['segment_type']
+        min_threshold = options['min_threshold']
+
+        if start_piece is None and end_piece is not None:
+            raise CommandError('--end requires --start.')
+
+        highest_piece_id = Piece.objects.order_by('-id').values_list('id', flat=True).first()
+        if highest_piece_id is None:
+            raise CommandError('No Piece rows exist.')
+
+        if start_piece is None and end_piece is None:
+            recent_piece_ids = list(
+                Piece.objects.order_by('-id').values_list('id', flat=True)[:10]
+            )
+            start_piece = recent_piece_ids[-1]
+            end_piece = recent_piece_ids[0]
+        elif end_piece is None:
+            end_piece = highest_piece_id
+
+        if start_piece > end_piece:
+            raise CommandError('--start must be less than or equal to --end.')
+
+        searcher = SimilaritySearcher(
+            start_piece=start_piece,
+            end_piece=end_piece + 1,
+            min_threshold=min_threshold,
+            segment_type=segment_type,
+        )
+        searcher.run_pieces()
